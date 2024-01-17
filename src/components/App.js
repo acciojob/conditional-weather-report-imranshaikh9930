bed69991b2c152f739aa46b1472189983625aeda
Imran shaikh <imranpathan7040@gmail.com>
2024-01-17 19:57:17 +0530

    temp update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import Whether from "./Whether";
 
 const App = () => {
+
+  const whetherData = { temperature: 25, conditions: "Sunny"}
   return (
     <div>
+      <Whether whether={whetherData}/>
         {/* Do not remove the main div */}
+
     </div>
   )
 }
---
+++ src/components/Whether.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Whether({whether}) {
+
+    const {temperature, conditions} = whether;
+
+    const aboveThrehold = temperature > 20;
+
+  return (
+    <div>
+        <h1>Whether App</h1>
+        <p>
+            Tempreture : <span style={{color: aboveThrehold ? "red":"blue"}}>{temperature}</span>
+        </p>
+        <p>Conditions: {conditions}</p>
+    </div>
+  )
+}
+
+export default Whether
